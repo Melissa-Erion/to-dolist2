b35e73f8b7d8b1664e9a4dc44ffc726091d8fc43
Melissa-Erion <melissaerion26@gmail.com>
2023-07-27 10:40:43 -0600

    obtained my api key. made GET request to check connection to the API server

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+$.ajax({
+    type: 'GET',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=274',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      console.log(response);
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+});
