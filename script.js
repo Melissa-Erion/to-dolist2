c3ff47794ee10779a1abac5be4903a5e1abe149e
Melissa-Erion <melissaerion26@gmail.com>
2023-07-27 10:54:28 -0600

    GET request into a function names getAndDisplayAllTasks, call it in the success callback of POST request. called function at the end of program

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
 $(document).ready(function() {
-$.ajax({
-    type: 'GET',
-    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=274',
-    dataType: 'json',
-    success: function (response, textStatus) {
-        response.tasks.forEach(function (task) {
-            $('#todo-list').append('<p>' + task.content + '</p>');
-          })
-        },
-    error: function (request, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
+    var getAndDisplayAllTasks = function () {
+        $.ajax({
+          type: 'GET',
+          url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=274',
+          dataType: 'json',
+          success: function (response, textStatus) {
+            $('#todo-list').empty();
+            var filteredTasks = response.tasks;
+    
+            if (filterPreference === 'completed') {
+              filteredTasks = filteredTasks.filter(function(task) {
+                return task.completed;
+              });
+            } else if (filterPreference === 'active') {
+              filteredTasks = filteredTasks.filter(function(task) {
+                return !task.completed;
+              });
+            }
+    
+            filteredTasks.forEach(function (task) {
+              $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? ' checked' : '') + '></div>');
+            });
+          },
+          error: function (request, textStatus, errorMessage) {
+            console.log(errorMessage);
+          }
+        });
+      };
+
+  var createTask = function () {
+    $.ajax({
+      type: 'POST',
+      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=274',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#new-task-content').val() 
+        }
+      }),
+      success: function (response, textStatus) {
+        console.log(response);
+        $('#new-task-content').val('');
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  };
+
+  
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
   });
 });
